da8eda4d96bd46396820ed4dd5a2283d8ba7023d
David Moulton <dbmoulton@gmail.com>
2023-03-21 20:54:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email){
+    
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({name, email}),
+  };
+  
+   return fetch("http://localhost:3000/users", configurationObject)
+    .then((resp) => resp.json())
+    .then(function (object) {
+      console.log(object);
+      document.body.innerHTML = object.id
+    })
+    .catch(function (message) {
+        alert("Bad things!");
+        document.body.innerHTML = message.message
+    })
+}
